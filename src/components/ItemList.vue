a8fee458560037a32101d2c285562f3a1671beb3
Viktoriia Sarapii <viktoriia.sarapii@devit-team.com>
2020-11-11 14:37:35 +0200

    add font changes

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -10,31 +10,40 @@
     </template>
 
     <template v-else>
-    <v-list three-line>
-      <template v-for="(item) in itemsArray">
-        <v-list-item
-            :key="item.id"
-        >
-          <v-list-item-avatar>
-            <v-img :src="item.imagePath"></v-img>
-          </v-list-item-avatar>
+      <v-list three-line>
+        <template v-for="(item) in itemsArray">
+          <v-list-item
+              :key="item.id"
+          >
+            <v-list-item-avatar>
+              <v-img :src="item.imagePath"></v-img>
+            </v-list-item-avatar>
 
-          <v-list-item-content>
-            <v-list-item-title v-html="item.creatorName"></v-list-item-title>
-            <v-list-item-subtitle v-html="item.total"></v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </template>
-    </v-list>
+            <v-list-item-content>
+              <v-list-item-title>
+              <span v-bind:style="'font-size:'+fontSize+'px'">
+                {{ item.creatorName }}
+              </span>
+              </v-list-item-title>
+              <v-list-item-subtitle v-html="item.total">
+              <span v-bind:style="'font-size:'+((fontSize/100)*85)+'px'">
+                {{ item.total }}
+              </span>
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
     </template>
   </v-card>
 </template>
 
 <script>
 export default {
-name: "ItemList",
+  name: "ItemList",
   props: {
-    itemsArray: Array
+    itemsArray: Array,
+    fontSize: Number || 14
   },
   mounted() {
 
@@ -43,11 +52,11 @@ name: "ItemList",
 </script>
 
 <style scoped>
-  div.not-found-container {
-    height: 100%;
-    min-height: 310px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+div.not-found-container {
+  height: 100%;
+  min-height: 310px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
